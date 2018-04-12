e62da3002d47d39a855410e109f45175131e00ad
LilliamHaro <qualia950@gmail.com>
2018-04-12 10:40:36 -0500

    arreglando detalles

--- src/js/app.js
+++ src/js/app.js
@@ -4,23 +4,26 @@ $(document).ready(function(){
 	$('#oldClient').on('click', function(event) {
 		typeClient = 'registrado';
 		console.log(typeClient);
+		$('#btn-select').text('Estoy registrado(a)')
 	});
 
 	$('#newClient').on('click', function(event) {
 		typeClient = 'nuevo';
 		console.log(typeClient);
+		$('#btn-select').text('Soy nuevo(a)')
 	});
 
 	$('#btnClient').on('click', function(event) {
 		if(typeClient === 'registrado') {
 			$('#box-1').addClass('hide');
 			$('#box-2').removeClass('hide');
+			
 
 		} else if(typeClient === 'nuevo') {
 			 window.location.href = 'src/views/view3.html';
 
 		} else {
-			alert('por favor elija una opcion >:(')
+			alert('Por favor elija una opcion')
 		}
 	});
 })
